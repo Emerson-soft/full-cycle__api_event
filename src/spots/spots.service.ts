c005deb601ea6616180c0d1850c655da12d74a12
Emerson Mendonça <emerson_10_s@hotmail.com>
2024-06-20 20:42:38 -0300

    feat: crie o model de sport

--- src/spots/spots.service.ts
+++ src/spots/spots.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSpotDto } from './dto/create-spot.dto';
 import { UpdateSpotDto } from './dto/update-spot.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SpotsService {
+  constructor(private prismaService: PrismaService) {}
+
   create(createSpotDto: CreateSpotDto) {
     return 'This action adds a new spot';
   }
 
-  findAll() {
+  findAll(eventId: string) {
     return `This action returns all spots`;
   }
 
-  findOne(id: number) {
+  findOne(eventId: string, sportId: number) {
     return `This action returns a #${id} spot`;
   }
 
-  update(id: number, updateSpotDto: UpdateSpotDto) {
+  update(eventId: string, sportId: number, updateSpotDto: UpdateSpotDto) {
     return `This action updates a #${id} spot`;
   }
 
-  remove(id: number) {
+  remove(eventId: string, sportId: number) {
     return `This action removes a #${id} spot`;
   }
 }
